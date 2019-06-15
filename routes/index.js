2af61c8e461d93d35b632c5fe44a28cb15fc1324
Aditya <adityashnkr5@gmail.com>
2019-06-15 14:49:30 +0530

    Add dynamic styles/scripts to ejs

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ const router = express.Router();
 
 /* GET home page. */
 router.get("/", (req, res, next) => {
-	res.render("index", { title: "Blog Post" });
+	res.render("index", {
+		title: "Blog Post",
+		styles: {
+			custom: ["/stylesheets/slider.css"]
+		}
+	});
 });
 
 module.exports = router;
