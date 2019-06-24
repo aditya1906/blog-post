4f46dde80a0292df67db1dadecd1892203a3a70e
Aditya <adityashnkr5@gmail.com>
2019-06-24 17:33:15 +0530

    Add category to models and substr to description

--- models/blog.js
+++ models/blog.js
@@ -1,9 +1,10 @@
 const mongoose = require("mongoose");
 
 const blogSchema = new mongoose.Schema({
-    name: String,
-    image: String,
-    description: String
+	name: String,
+	image: String,
+	category: String,
+	description: String
 });
 
 const blog = mongoose.model("blog", blogSchema);
